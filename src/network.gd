2e7b27f5dd2b18b293743df2ee24765609e636bf
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-12-07 23:22:54 +0200

    Fix modulation
    Detection is still not working properly

--- src/network.gd
+++ src/network.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 				var atlas_coords := tile_map.get_cell_atlas_coords(0, coords)
 				if atlas_coords.y < 1:
 					continue
-				check_if_active(tile_data, coords)
+				check_if_active(tile_data, coords, true)
 
 
 func _input(event: InputEvent) -> void:
@@ -31,11 +31,10 @@ func _input(event: InputEvent) -> void:
 					alternative_tile = 0
 				tile_map.set_cell(0, coords, 1, atlas_coords, alternative_tile)
 				var new_tile_data := tile_map.get_cell_tile_data(0, coords)
-				check_if_active(new_tile_data, coords)
+				check_if_active(new_tile_data, coords, false)
 
 
-func check_if_active(tile_data: TileData, coords: Vector2i) -> void:
-	#var active := tile_data.get_custom_data("activated") as bool
+func check_if_active(tile_data: TileData, coords: Vector2i, start_of_game: bool) -> void:
 	var look_at_1 := tile_data.get_custom_data("look_at_1") as Vector2i
 	var look_at_2 := tile_data.get_custom_data("look_at_2") as Vector2i
 	var look_at_3 := tile_data.get_custom_data("look_at_3") as Vector2i
@@ -44,21 +43,27 @@ func check_if_active(tile_data: TileData, coords: Vector2i) -> void:
 	var neighbor_2 := tile_map.get_cell_tile_data(0, coords + look_at_2)
 	var neighbor_3 := tile_map.get_cell_tile_data(0, coords + look_at_3)
 	var neighbor_4 := tile_map.get_cell_tile_data(0, coords + look_at_4)
-	var active_neighbor_1 := false
-	if is_instance_valid(neighbor_1):
-		active_neighbor_1 = neighbor_1.get_custom_data("activated")
-	var active_neighbor_2 := false
-	if is_instance_valid(neighbor_2):
-		active_neighbor_2 = neighbor_2.get_custom_data("activated")
-	var active_neighbor_3 := false
-	if is_instance_valid(neighbor_3):
-		active_neighbor_3 = neighbor_3.get_custom_data("activated")
-	var active_neighbor_4 := false
-	if is_instance_valid(neighbor_4):
-		active_neighbor_4 = neighbor_4.get_custom_data("activated")
+	var active_neighbor_1 := get_neighbor_status(neighbor_1, coords + look_at_1, start_of_game)
+	var active_neighbor_2 := get_neighbor_status(neighbor_2, coords + look_at_2, start_of_game)
+	var active_neighbor_3 := get_neighbor_status(neighbor_3, coords + look_at_3, start_of_game)
+	var active_neighbor_4 := get_neighbor_status(neighbor_4, coords + look_at_4, start_of_game)
 	var is_active := active_neighbor_1 or active_neighbor_2 or active_neighbor_3 or active_neighbor_4
-	tile_data.set_custom_data("activated", is_active)
+	var alternative_tile := tile_map.get_cell_alternative_tile(0, coords)
+	var atlas_coords := tile_map.get_cell_atlas_coords(0, coords)
 	if is_active:
-		tile_data.modulate = Color.WHITE
+		atlas_coords.y = 1
+		tile_map.set_cell(0, coords, 1, atlas_coords, alternative_tile)
 	else:
-		tile_data.modulate = Color.GRAY
+		atlas_coords.y = 2
+		tile_map.set_cell(0, coords, 1, atlas_coords, alternative_tile)
+
+
+func get_neighbor_status(neighbor_data: TileData, neighbor_coords: Vector2i, start_of_game: bool) -> bool:
+	if not is_instance_valid(neighbor_data):
+		return false
+	var atlas_coords := tile_map.get_cell_atlas_coords(0, neighbor_coords)
+	if atlas_coords == Vector2i.ZERO or atlas_coords == Vector2i(1, 0):
+		return true
+	if not start_of_game and atlas_coords.y == 1:
+		return true
+	return false
