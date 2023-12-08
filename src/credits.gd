35767c080ab50aa7121d38af2ff80388a8e2ca0d
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-12-08 21:13:51 +0200

    Add credits and exit game

---
+++ src/credits.gd
@@ -0,0 +1,11 @@
+extends Panel
+
+var menu := load("res://src/menu.tscn") as PackedScene
+
+
+func _ready() -> void:
+	%ReturnToMenu.grab_focus()
+
+
+func _on_return_to_menu_pressed() -> void:
+	get_tree().change_scene_to_packed(menu)
--- src/menu.gd
+++ src/menu.gd
@@ -1,6 +1,8 @@
 extends Panel
 
 const NETWORK_TSCN := preload("res://src/main_game.tscn")
+const CREDITS_TSCN := preload("res://src/credits.tscn")
+
 @onready var play_button: Button = %PlayButton
 @onready var exit_button: Button = %ExitButton
 
@@ -13,3 +15,19 @@ func _ready() -> void:
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(NETWORK_TSCN)
+
+
+func _on_hi_scores_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_packed(CREDITS_TSCN)
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
