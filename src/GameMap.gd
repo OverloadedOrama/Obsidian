bc3effef65514c35d632e7184e267187037cf980
Emmanouil Papadeas <manoschool@yahoo.gr>
2023-12-09 03:13:34 +0200

    Made game be 9x9

--- src/GameMap.gd
+++ src/GameMap.gd
@@ -1,7 +1,7 @@
 class_name GameMap
 extends TileMap
 
-@export var grid_size := Vector2i(8, 9)
+@export var grid_size := Vector2i(9, 9)
 @export var water_targets_needed := 2
 @export var lava_targets_needed := 2
 
